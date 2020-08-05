a65b7a1d4db90218f4c6d1293c47cfc427bddc00
Aibek <nomad.aibek@gmail.com>
2020-08-05 21:45:51 +0600

    Added models Issue, IssueType, IssueStatus

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class IssueType(models.Model):
+    type = models.CharField(max_length=40, null=False, blank=False, verbose_name='Type')
+
+
+class IssueStatus(models.Model):
+    status = models.CharField(max_length=40, null=False, blank=False, verbose_name='Status')
+
+
+class Issue(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Title')
+    description = models.CharField(max_length=2000, null=True, blank=True, verbose_name='Description')
+    status = models.ForeignKey('webapp.IssueStatus', related_name='statuses', on_delete=models.PROTECT,
+                               verbose_name='Status')
+    type = models.ForeignKey('webapp.IssueType', related_name='types', on_delete=models.PROTECT, verbose_name='Type')
+    create_at = models.DateField(auto_now_add=True, verbose_name='Create date')
+    updated_at = models.DateField(auto_now=True, verbose_name='Updated date')
+
+    def __str__(self):
+        return "{}. {}".format(self.title, self.type)
+
+    class Meta:
+        verbose_name = 'Issue'
+        verbose_name_plural = 'Issues'
