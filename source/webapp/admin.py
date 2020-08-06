8b4abda892b0e67b7ef7bd3c7ce85a7ac44a33d5
Aibek <nomad.aibek@gmail.com>
2020-08-06 22:39:39 +0600

    added super user

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from webapp.models import Issue, IssueType, IssueStatus
 
 
-admin.site.register(Issue)
-admin.site.register(IssueStatus)
-admin.site.register(IssueType)
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title')
+    list_display_links = ('pk', 'title')
+    search_fields = ('title',)
+
+
+class IssueStatusAdmin(admin.ModelAdmin):
+    list_display = ('status',)
+
+
+class IssueTypesAdmin(admin.ModelAdmin):
+    list_display = ('type',)
+
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(IssueStatus, IssueStatusAdmin)
+admin.site.register(IssueType, IssueTypesAdmin)
